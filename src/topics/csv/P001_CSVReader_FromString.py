eeaedb3c82ab04429139b3d191af8416ec6bc4f1
Vikash <vikash.kmc@gmail.com>
2016-01-04 18:02:39 +0530

    Add code example to read CSV from a string.

---
+++ src/topics/csv/P001_CSVReader_FromString.py
@@ -0,0 +1,17 @@
+import csv
+
+# Step 1: CSV String
+csvString = "Name,Debut,Team\n" \
+            "Sachin Tendulkar,1989,India\n" \
+            "Rahul Dravid,1996,India\n" \
+            "Saurav Ganguly,1996,India"
+
+# Step 2: Convert CSV string to List
+csvString = csvString.split('\n');
+
+# Step 3: Initialise CSV Reader
+csvReader = csv.reader(csvString)
+
+# Step 4: Traverse CSV list row wise
+for row in csvReader:
+    print row
