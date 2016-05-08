4c9f779ca686e66f80179ff484246cada997b51e
Vikash <vikash.kmc@gmail.com>
2016-05-08 23:34:10 +0530

    Add example of logging in Python.

--- src/topics/regex/P014_ReModule_OptionFlags.py
+++ src/topics/regex/P014_ReModule_OptionFlags.py
@@ -30,4 +30,5 @@ re.X or re.VERBOSE      This flag allows you to write regular expressions that l
 a = re.compile(r"""\d +  # the integral part
                    \.    # the decimal point
                    \d *  # some fractional digits""", re.X)
+
 b = re.compile(r"\d+\.\d*")
