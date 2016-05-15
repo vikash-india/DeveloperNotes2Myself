27bf65e08cd251194d0e2b6b012115c4c1bb1bc4
Vikash <vikash.kmc@gmail.com>
2016-05-15 22:30:16 +0530

    Update example.

--- src/concepts/P004_Comments.R
+++ src/concepts/P004_Comments.R
@@ -3,6 +3,7 @@
 # Note
 # 1. Comments can be created using # character.
 # 2. Anything to the right of the # (including the # itself) is ignored.
+# 3. R does not support multi-line comments or comment blocks.
 
 # This is an example of a comment
 print("Comments can be created using #")
--- src/concepts/P006_AssignmentOperator.R
+++ src/concepts/P006_AssignmentOperator.R
@@ -2,6 +2,5 @@
 
 # Note
 # 1. The <- symbol is the assignment operator.
-# 2. 
 message <- "Hello World"
 print(message)
