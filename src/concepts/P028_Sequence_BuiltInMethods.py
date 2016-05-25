580d71a9a30b701ac2d906baf7a5af8e75476da2
Vikash <vikash.kmc@gmail.com>
2016-05-26 00:30:20 +0530

    Add examples on list.

---
+++ src/concepts/P028_Sequence_BuiltInMethods.py
@@ -0,0 +1,9 @@
+# Description: Sequence Built-in Methods
+
+# Sequence Methods
+word = 'Hello'
+
+print(len(word[1:3]))       # 2
+print(ord('A'))             # 65
+print(chr(65))              # A
+print(str(65))              # 65
