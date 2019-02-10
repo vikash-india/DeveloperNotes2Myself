1b8e3bbadef5b70b0cb073d6d10c90b6dfcb1f5a
Vikash <vikash.kmc@gmail.com>
2019-02-11 00:23:22 +0530

    Add direct component.

---
+++ languages/java/src/topics/camel/P010_README.md
@@ -0,0 +1,36 @@
+# Description: Transform Messages Using Processors and Direct Component
+
+### About
+* This example will transform CSV file with comma (,) as separator to colon (:) as separator.
+* Example: CSV files of format `Dilbert,Single,Male,Engineer` will be transformed to `Dilbert:Single:Male:Engineer`.
+
+### Libraries Needed for This Code
+* camel-core.jar
+* slf4j-1.7.25/slf4j-api-1.7.25.jar
+* apache-log4j-1.2.17/log4j-1.2.17.jar
+
+### Files Directory Structure Needed
+1. The following directories should exist
+    - /tmp/input
+    - /tmp/output
+    
+2. The `/tmp/input` directory should have a few sample files
+    - file1.csv with content as `Dilbert,Single,Male,Engineer`
+
+### Bash Script for Creating Files and Directory Structure
+```
+# Create Directory Structure
+mkdir -p /tmp/input /tmp/output
+
+# Create Files
+touch /tmp/input/f1.csv
+echo "Dilbert,Single,Male,Engineer" > /tmp/input/f1.csv
+echo "Catbert,Unknown,Male,HR Director" >> /tmp/input/f1.csv 
+
+# Remove files from output directory
+rm /tmp/output/*
+
+# List Directories and Content
+ll /tmp/input/ /tmp/output/
+cat /tmp/input/* /tmp/output/*
+```
---
+++ languages/java/src/topics/camel/P010_TransformUsingProcessorAndDirectComponent.java
@@ -0,0 +1,68 @@
+package topics.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.log4j.PropertyConfigurator;
+
+public class P010_TransformUsingProcessorAndDirectComponent {
+
+    public static void main(String[] args) {
+        // Enable log4J Logging
+        String log4jPropertiesPath = "src/topics/camel/P004_log4j.properties";
+        PropertyConfigurator.configure(log4jPropertiesPath);
+
+        CamelContext context = new DefaultCamelContext();
+
+        try {
+            // Use Customer RouteBuilder
+            context.addRoutes(new CustomCamelFileProcessorRouteBuilder2());
+
+            context.start();
+            Thread.sleep(5000);
+            context.stop();
+        } catch (Exception e) {
+            System.out.println("Inside Exception : " + e);
+        }
+    }
+}
+
+// Implement Custom Camel Processor Using a Direct Component
+class CustomCamelFileProcessor2 implements Processor {
+    public void process(Exchange exchange) throws Exception {
+        System.out.println("Exchange in Processor: " + exchange.getIn().getBody());
+
+        try {
+            String oldLine = (String) exchange.getIn().getBody();
+            String newLine = oldLine.replace(",", ":");
+
+            System.out.println("Old Line: " + oldLine);
+            System.out.println("New Line: " + newLine);
+
+            exchange.getIn().setBody(newLine);
+        } catch (Exception e) {
+            System.out.println("Exception in processor is : " + e);
+        }
+    }
+}
+
+// Implement Custom Camel RouteBuilder Using Direct Component
+class CustomCamelFileProcessorRouteBuilder2 extends RouteBuilder {
+
+    public void configure() throws Exception {
+        // TODO:
+        // 1. Figure out how to use "direct:processorInput" instead of "file:..."
+        // 2. Figure out how to given an input to direct component instead of convertBodyTo so that
+        //    each line is processed separately instead of a single string.
+        from("file:/tmp/input?noop=true")
+                // Convert File to a String
+                .convertBodyTo(String.class)
+                .log("Message BEFORE process is ${body} and Headers are ${headers}")
+                // Use Custom Camel Processor 2
+                .process(new CustomCamelFileProcessor2())
+                .log("Message AFTER process is ${body} and Headers are ${headers}")
+                .to("file:/tmp/output?fileName=output.txt");
+    }
+}
