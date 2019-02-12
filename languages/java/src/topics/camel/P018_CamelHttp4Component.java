18e0b84ae91488413c01f2b0c52a8c517e8a24b9
Vikash <vikash.kmc@gmail.com>
2019-02-12 23:18:50 +0530

    Add code for camel http4 component

---
+++ languages/java/src/topics/camel/P018_CamelHttp4Component.java
@@ -0,0 +1,45 @@
+package topics.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.log4j.PropertyConfigurator;
+
+public class P018_CamelHttp4Component {
+
+    public static void main(String[] args) {
+        // Enable log4J Logging
+        String log4jPropertiesPath = "src/topics/camel/P004_log4j.properties";
+        PropertyConfigurator.configure(log4jPropertiesPath);
+
+        CamelContext context = new DefaultCamelContext();
+
+        try {
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("direct:start")
+                            // HTTP4 can be only be in to() route.
+                            .to("http4:example.com")
+                            .log("Received Message is ${body} and Headers are ${headers}");
+                }
+            });
+
+            context.start();
+
+            // Send a direct start to begin processing
+            ProducerTemplate producer = context.createProducerTemplate();
+            Exchange exchange = new DefaultExchange(context);
+            producer.send("direct:start", exchange);
+
+            Thread.sleep(5000);
+
+            context.stop();
+        } catch (Exception e) {
+            System.out.println("Exception : " + e);
+        }
+    }
+}
---
+++ languages/java/src/topics/camel/P018_README.md
@@ -0,0 +1,17 @@
+# Description: Camel HTTP4 Component
+
+### About
+* This example will fetch a file from `http://example.com` using camel HTTP4 component.
+
+### Libraries Needed
+* camel-core.jar
+* slf4j-api.jar
+* log4j.jar
+* [For HTTP4] camel-http-common.jar
+* [For HTTP4] camel-http4.jar      
+* [For HTTP4] commons-logging.jar     
+* [For HTTP4] httpclient.jar
+* [For HTTP4] httpcore.jar
+
+### TODO
+* None
