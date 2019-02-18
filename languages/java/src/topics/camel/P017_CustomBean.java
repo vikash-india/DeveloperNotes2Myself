3e91d76a97cbe37861622b96c18d5d50ae13560b
Vikash <vikash.kmc@gmail.com>
2019-02-18 23:52:46 +0530

    Rename objects.

--- languages/java/src/topics/camel/P017_CustomBean.java
+++ languages/java/src/topics/camel/P017_CustomBean.java
@@ -1,5 +1,7 @@
 package topics.camel;
 
+import org.apache.camel.Exchange;
+
 // from(direct:start).to(P017_CustomBean.class)
 public class P017_CustomBean {
     public void doSomething(Exchange exchange) {
